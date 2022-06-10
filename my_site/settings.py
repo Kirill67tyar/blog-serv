fa8415c800005f8ae7d7b51037f1dd91b6974405
Kirill Bogomolov <kirillbogomolov.ric@gmail.com>
2022-06-10 17:46:05 +0300

    asda

--- my_site/settings.py
+++ my_site/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'postgres',
-        'PASSWORD': '10789007qw_alskdjfhg',
+        'PASSWORD': PASSWORD,
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
